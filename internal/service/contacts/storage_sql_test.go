1290c708e0a7fd1826d541775c728fd511fbda41
Peltoche <pierre.peltier@protonmail.com>
2024-05-14 11:47:52 +0200

    Add some tests for the contact storage

--- internal/service/contacts/storage_sql_test.go
+++ internal/service/contacts/storage_sql_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/Peltoche/halium/internal/tools/sqlstorage"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -24,5 +25,51 @@ func TestContactSqlStorage(t *testing.T) {
 
 		// Asserts
 		require.NoError(t, err)
+	})
+
+	t.Run("GetByID success", func(t *testing.T) {
+		// Run
+		res, err := store.GetByID(ctx, contact.id)
+
+		// Asserts
+		require.NoError(t, err)
+		assert.Equal(t, contact, res)
+	})
+
+	t.Run("Patch success", func(t *testing.T) {
+		// Run
+		err := store.Patch(ctx, contact, map[string]any{
+			"surname": "changed",
+		})
+
+		// Asserts
+		require.NoError(t, err)
+	})
+
+	t.Run("GetByID the patched version", func(t *testing.T) {
+		// Run
+		res, err := store.GetByID(ctx, contact.id)
+
+		// Asserts
+		require.NoError(t, err)
+		assert.NotEqual(t, contact, res)
+		assert.Equal(t, "changed", res.name.surname)
+	})
+
+	t.Run("Delete success", func(t *testing.T) {
+		// Run
+		err := store.Delete(ctx, contact.id)
+
+		// Asserts
+		require.NoError(t, err)
+	})
+
+	t.Run("GetByID not found", func(t *testing.T) {
+		// Run
+		res, err := store.GetByID(ctx, contact.id)
+
+		// Asserts
+		require.ErrorIs(t, err, errNotFound)
+		assert.Nil(t, res)
 	})
 }
